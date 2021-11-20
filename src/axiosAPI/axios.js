f511ae50a00c1b9f994ad0512232552fdd0e1518
OengKimChhay <71259275+OengKimChhay@users.noreply.github.com>
2021-11-20 17:24:56 +0700

    Revert "docker"

--- src/axiosAPI/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-//------------create environment---------------
-export default axios.create({
-  baseURL: "http://localhost:8000/api",
-  headers: {
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer '+localStorage.getItem('access_token')  //this is how to sent bare token to api
-  }
-});
