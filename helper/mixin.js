d921d3207a69e80da1c5b518df7704ae7c8f005a
Oengkimchhay <chihe333@gmail.com>
2021-11-28 23:29:07 +0700

    s

--- helper/mixin.js
+++ helper/mixin.js
@@ -9,7 +9,7 @@ const mixin =  {
             usertype: localStorage.getItem('usertype'),
             userid: localStorage.getItem('userid'),
             userimage: localStorage.getItem('userimage'),
-            ImageURL: "http://127.0.0.1:8000/images"
+            ImageURL: "http://188.166.220.161:1027/images"
         }
     },
     methods: {
--- src/axiosAPI/axios.js
+++ src/axiosAPI/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 //------------create environment---------------
 export default axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "http://188.166.220.161:1027/api",
   headers: {
     'Content-Type': 'application/json',
     'Authorization': 'Bearer '+localStorage.getItem('access_token')  //this is how to sent bare token to api
